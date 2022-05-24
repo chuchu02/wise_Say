814cdab1b6778bf5fbe1db088be546ba595f1ea9
chuchu02 <rlaxogns73@gmail.com>
2022-05-24 11:54:11 +0900

    생성하기

--- app.js
+++ app.js
@@ -14,8 +14,76 @@ const pool = mysql.createPool({
 });
 
 const app = express();
+app.use(express.json());
+
+const corsOptions = {
+  origin: "https://cdpn.io",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
+app.use(cors(corsOptions));
+
 const port = 3000;
 
+// 생성 시작
+app.post("/todos/:id", async (req, res) => {
+  // const id = req.params.id;
+  const { id } = req.params;
+
+  const [rows] = await pool.query(
+    `
+  SELECT * 
+  FROM todo 
+  WHERE id = ?
+  `,
+    [id]
+  );
+
+  if (rows.length == 0) {
+    res.status(404).json({
+      msg: "not found",
+    });
+    return;
+  }
+
+  const { reg_date, is_completed, perform_date, content } = req.body;
+  if (!reg_date) {
+    res.status(400).json({
+      msg: "reg_date required",
+    });
+    return;
+  }
+  if (!perform_date) {
+    res.status(400).json({
+      msg: "perfom_date required",
+    });
+    return;
+  }
+  if (!content) {
+    res.status(400).json({
+      msg: "content required",
+    });
+    return;
+  }
+
+  const [rs] = await pool.query(
+    `
+  INSERT INTO todo
+  SET reg_date = ?,
+  perform_date = ?,
+  is_completed = ?,
+  content = ?
+  `,
+    [reg_date, perform_date, is_completed, content]
+  );
+
+  res.status(201).json({
+    id: rs.insertId,
+  });
+});
+// 생성 끝
+
+//단건조회 시작
 app.get("/wiseSayings/:id", async (req, res) => {
   //const id = req.params.id;
   const { id } = req.params;
@@ -40,7 +108,9 @@ app.get("/wiseSayings/:id", async (req, res) => {
 });
 
 app.get("/wiseSayings", async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM wiseSaying ORDER BY id ASC");
+  const [rows] = await pool.query(
+    "SELECT * FROM wiseSaying ORDER BY RAND() LIMIT 1"
+  );
 
   res.json(rows);
 });
